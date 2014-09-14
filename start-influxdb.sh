fd0fa30607a8c3e198240227da18f7e7f91cd5a9
John Van Gorp <john.vangorp@gmail.com>
2014-09-14 12:01:18 -0700

    Config and bash script updates.

--- start-influxdb.sh
+++ start-influxdb.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-docker run -it -volumes-from data ubuntu:trusty /bin/bash
+docker rm influxdb
+docker run -d --name influxdb \
+	--volumes-from data \
+	-p 8083:8083 -p 8086:8086 \
+	jvangorp/influxdb
